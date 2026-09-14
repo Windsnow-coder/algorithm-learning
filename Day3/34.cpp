84baaf7be1df964bd07f75dfbfc3e53b65553ed7
Liu <3077402600@qq.com>
2026-09-14 15:05:13 +0800

    Day3 solve leetcode 34

---
+++ Day3/34.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    vector<int> searchRange(vector<int>& nums, int target) {
+        vector<int> n(2,-1);
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)
+            {
+                n[0]=mid;
+                r=mid-1;
+            }
+            else if(nums[mid]<target)l=mid+1;
+            else if(nums[mid]>target)r=mid-1;
+        }
+        l=0;
+        r=nums.size()-1;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)
+            {
+                n[1]=mid;
+                l=mid+1;
+            }
+            else if(nums[mid]<target)l=mid+1;
+            else if(nums[mid]>target)r=mid-1;
+        }
+        return n;
+    }
+};
