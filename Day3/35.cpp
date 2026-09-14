4b9eb3bdfcdfc9a7b9b8aa1e7f19597ec5f86d02
Liu <3077402600@qq.com>
2026-09-14 13:52:16 +0800

    Day3 solve leetcode 35

---
+++ Day3/35.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        int l=0;
+        int r=nums.size()-1;
+        while(l<=r)
+        {
+            int mid=l+(r-l)/2;
+            if(nums[mid]==target)return mid;
+            else if(nums[mid]<target)l=mid+1;
+            else if(nums[mid]>target)r=mid-1;
+        }
+        return l;//标准二分没找到时，l往往就是插入位置
+    }
+};
